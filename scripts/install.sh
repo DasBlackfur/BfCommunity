a8d21bad4d8991ca125c6862d168e2991eb392e2
blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2023-06-07 20:22:33 +0200

    hello??

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 FORGE_INSTALLER_URL="https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.4-45.0.66/forge-1.19.4-45.0.66-installer.jar"
-DEB_DEPENDENCIES=(openjdk-17-jre-headless python3 cargo)
+DEB_DEPENDENCIES=( openjdk-17-jre-headless python3 cargo )
 
 sudo apt update
 
