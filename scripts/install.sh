6cbc8e40d535720b4889f15bc9f73c455ec843d9
blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2023-06-07 19:15:36 +0200

    fixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export FORGE_INSTALLER_URL="https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.4-45.0.66/forge-1.19.4-45.0.66-installer.jar"
-export DEB_DEPENDENCIES=(
+FORGE_INSTALLER_URL="https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.4-45.0.66/forge-1.19.4-45.0.66-installer.jar"
+DEB_DEPENDENCIES=(
   "openjdk-17-jre-headless"
   "python3"
   "cargo"
@@ -8,7 +8,7 @@ export DEB_DEPENDENCIES=(
 
 sudo apt update
 
-for n in $(DEB_DEPENDENCIES)
+for n in "${DEB_DEPENDENCIES[@]}"
 do
   sudo apt install "$n"
 done
