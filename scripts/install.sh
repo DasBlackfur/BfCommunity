97fdbf14b5db60e4c61fdc06a7a5e90da06382b2
blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2023-06-07 20:48:16 +0200

    other rust install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 FORGE_INSTALLER_URL="https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.4-45.0.66/forge-1.19.4-45.0.66-installer.jar"
-DEB_DEPENDENCIES=( openjdk-17-jre-headless python3 cargo curl )
+DEB_DEPENDENCIES=( openjdk-17-jre-headless python3 curl )
 
 sudo apt update
 
@@ -9,7 +9,10 @@ do
   sudo apt install -y "$n"
 done
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
 cargo install --git https://github.com/Storyyeller/Krakatau.git --branch v2
 
 mkdir -p "ServerInstance"
+rm ServerInstance/forge-installer.jar
 curl $FORGE_INSTALLER_URL -o ServerInstance/forge-installer.jar
