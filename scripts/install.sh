f6e61b9285213dc7e0528b2da4452cbd492092b2
blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2023-06-07 20:27:50 +0200

    i am retart

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 FORGE_INSTALLER_URL="https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.4-45.0.66/forge-1.19.4-45.0.66-installer.jar"
-DEB_DEPENDENCIES=( openjdk-17-jre-headless python3 cargo )
+DEB_DEPENDENCIES=( openjdk-17-jre-headless python3 cargo curl )
 
 sudo apt update
 
 for n in "${DEB_DEPENDENCIES[@]}"
 do
-  sudo apt install "$n"
+  sudo apt install -y "$n"
 done
 
-cargo install --git https://github.com/Storyyeller/Krakatau.git
+cargo install --git https://github.com/Storyyeller/Krakatau.git --branch v2
 
 mkdir "ServerInstance"
 curl $FORGE_INSTALLER_URL -o ServerInstance/forge-installer.jar
