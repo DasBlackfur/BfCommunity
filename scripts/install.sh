e4bfe6a23274e192af5166916306318adfe852ca
blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2023-06-07 19:14:13 +0200

    Progress

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 export FORGE_INSTALLER_URL="https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.4-45.0.66/forge-1.19.4-45.0.66-installer.jar"
 export DEB_DEPENDENCIES=(
   "openjdk-17-jre-headless"
@@ -5,4 +6,14 @@ export DEB_DEPENDENCIES=(
   "cargo"
 )
 
+sudo apt update
+
+for n in $(DEB_DEPENDENCIES)
+do
+  sudo apt install "$n"
+done
+
+cargo install --git https://github.com/Storyyeller/Krakatau.git
+
+mkdir "ServerInstance"
 curl $FORGE_INSTALLER_URL -o ServerInstance/forge-installer.jar
