84b14ff1ad53058aee92057994ba63c7a0e3e27c
blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2023-06-09 17:21:20 +0200

    added eula thing

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,15 +2,15 @@
 FORGE_INSTALLER_URL="https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.4-45.0.66/forge-1.19.4-45.0.66-installer.jar"
 DEB_DEPENDENCIES=( openjdk-17-jre-headless python3 curl )
 
-sudo apt update
+echo "BY RUNNING THIS INSTALLER YOU ACCEPT THE MINECRAFT EULA!"
 
+sudo apt update
 for n in "${DEB_DEPENDENCIES[@]}"
 do
   sudo apt install -y "$n"
 done
 
 command -v rustc >/dev/null 2>&1 || { curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y; source "$HOME/.cargo/env"; }
-
 cargo install --git https://github.com/Storyyeller/Krakatau.git --branch v2
 
 rm -rf ~/ServerInstance
@@ -18,3 +18,5 @@ mkdir -p ~/ServerInstance
 
 curl $FORGE_INSTALLER_URL -o ~/ServerInstance/forge-installer.jar
 java -jar ~/ServerInstance/forge-installer.jar --installServer ~/ServerInstance
+
+echo "eula=true" > ~/ServerInstance/eula.txt
